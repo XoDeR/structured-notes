9ebbb322b9a1f2db4493c4ff6b0380434213abe4
XoDeR <xoder.vs@gmail.com>
2025-11-20 17:57:16 +0100

    get env vars from .env file

--- backend/main.go
+++ backend/main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"structured-notes/logger"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	logger.Info("Initializing SN backend...")
+
+	// get env vars
+	err := godotenv.Load()
+	if err != nil {
+		logger.Error("Error loading .env file")
+	}
+
+	testEnvVar := os.Getenv("TEST")
+	fmt.Println("TEST:", testEnvVar)
 
 	fmt.Println("Structured Notes backend is running...")
 }
