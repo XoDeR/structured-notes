91edc17a96e62bb0ee703b38de81efc2ee96dc02
XoDeR <xoder.vs@gmail.com>
2025-11-25 19:01:28 +0100

    log service

--- backend/services/log.service.go
+++ backend/services/log.service.go
@@ -1,1 +1,53 @@
 package services
+
+import (
+	"structured-notes/models"
+	"structured-notes/repositories"
+	"structured-notes/types"
+	"structured-notes/utils"
+	"time"
+)
+
+type LogService interface {
+	GetConnections(userId types.Snowflake) ([]*models.Log, error)
+	GetLastConnection(userId types.Snowflake) (*models.Log, error)
+	CreateConnectionLog(userId types.Snowflake, ipAddr, userAgent, location, logType string) error
+	DeleteOldLogs() error
+}
+
+type logService struct {
+	logRepo   repositories.LogRepository
+	snowflake *utils.Snowflake
+}
+
+func NewLogService(logRepo repositories.LogRepository, snowflake *utils.Snowflake) LogService {
+	return &logService{
+		logRepo:   logRepo,
+		snowflake: snowflake,
+	}
+}
+
+func (s *logService) GetConnections(userId types.Snowflake) ([]*models.Log, error) {
+	return s.logRepo.GetByUserID(userId)
+}
+
+func (s *logService) GetLastConnection(userId types.Snowflake) (*models.Log, error) {
+	return s.logRepo.GetLastByUserID(userId)
+}
+
+func (s *logService) CreateConnectionLog(userId types.Snowflake, ipAddr, userAgent, location, logType string) error {
+	log := &models.Log{
+		Id:        s.snowflake.Generate(),
+		UserId:    userId,
+		IpAddr:    ipAddr,
+		UserAgent: userAgent,
+		Location:  location,
+		Type:      logType,
+		Timestamp: time.Now().UnixMilli(),
+	}
+	return s.logRepo.Create(log)
+}
+
+func (s *logService) DeleteOldLogs() error {
+	return s.logRepo.DeleteOld()
+}
