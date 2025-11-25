eb29da4e9d7a9414d1736f3e654fa88bd78f8609
XoDeR <xoder.vs@gmail.com>
2025-11-25 18:19:20 +0100

    user service

--- backend/services/user.service.go
+++ backend/services/user.service.go
@@ -1,1 +1,146 @@
 package services
+
+import (
+	"errors"
+	"structured-notes/models"
+	"structured-notes/repositories"
+	"structured-notes/types"
+	"structured-notes/utils"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserService interface {
+	GetAllUsers() ([]*models.User, error)
+	GetUserById(id types.Snowflake) (map[string]interface{}, error)
+	GetUserByUsername(username string) (*models.User, error)
+	SearchPublicUsers(query string) ([]*models.User, error)
+	CreateUser(username, firstname, lastname, avatar, email, password string) (*models.User, error)
+	UpdateUser(id types.Snowflake, firstname, lastname, avatar, email *string) (*models.User, error)
+	UpdatePassword(id types.Snowflake, newPassword string) error
+	DeleteUser(id types.Snowflake, mediaService MediaService) error
+}
+
+type userService struct {
+	userRepo  repositories.UserRepository
+	logRepo   repositories.LogRepository
+	snowflake *utils.Snowflake
+}
+
+func NewUserService(userRepo repositories.UserRepository, logRepo repositories.LogRepository, snowflake *utils.Snowflake) UserService {
+	return &userService{
+		userRepo:  userRepo,
+		logRepo:   logRepo,
+		snowflake: snowflake,
+	}
+}
+
+func (s *userService) GetAllUsers() ([]*models.User, error) {
+	return s.userRepo.GetAll()
+}
+
+func (s *userService) GetUserById(id types.Snowflake) (map[string]interface{}, error) {
+	user, err := s.userRepo.GetByID(id)
+	if err != nil {
+		return nil, err
+	}
+	if user == nil {
+		return nil, errors.New("user not found")
+	}
+
+	lastConnection, _ := s.logRepo.GetLastByUserID(id)
+
+	return map[string]interface{}{
+		"user":            user,
+		"last_connection": lastConnection,
+	}, nil
+}
+
+func (s *userService) GetUserByUsername(username string) (*models.User, error) {
+	return s.userRepo.GetByUsername(username)
+}
+
+func (s *userService) SearchPublicUsers(query string) ([]*models.User, error) {
+	return s.userRepo.SearchPublic(query)
+}
+
+func (s *userService) CreateUser(username, firstname, lastname, avatar, email, password string) (*models.User, error) {
+	exists, err := s.userRepo.CheckUsernameExists(username)
+	if err != nil {
+		return nil, err
+	}
+	if exists {
+		return nil, errors.New("username already exists")
+	}
+	if len(password) == 0 {
+		return nil, errors.New("password is required")
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, errors.New("failed to hash password")
+	}
+
+	user := &models.User{
+		Id:               s.snowflake.Generate(),
+		Username:         username,
+		Firstname:        &firstname,
+		Lastname:         &lastname,
+		Avatar:           &avatar,
+		Role:             1,
+		Email:            email,
+		Password:         string(hash),
+		CreatedTimestamp: time.Now().UnixMilli(),
+		UpdatedTimestamp: time.Now().UnixMilli(),
+	}
+
+	createdUser, err := s.userRepo.Create(user)
+	if err != nil {
+		return nil, err
+	}
+	createdUser.Password = ""
+	return createdUser, nil
+}
+
+func (s *userService) UpdateUser(id types.Snowflake, firstname, lastname, avatar, email *string) (*models.User, error) {
+	dbUser, err := s.userRepo.GetByID(id)
+	if err != nil || dbUser == nil {
+		return nil, errors.New("user not found")
+	}
+
+	user := &models.User{
+		Id:               id,
+		Username:         dbUser.Username,
+		Firstname:        utils.IfNotNilPointer(firstname, dbUser.Firstname),
+		Lastname:         utils.IfNotNilPointer(lastname, dbUser.Lastname),
+		Avatar:           utils.IfNotNilPointer(avatar, dbUser.Avatar),
+		Email:            utils.IfNotNilValue(email, dbUser.Email),
+		CreatedTimestamp: dbUser.CreatedTimestamp,
+		UpdatedTimestamp: time.Now().UnixMilli(),
+	}
+
+	return s.userRepo.Update(id, user)
+}
+
+func (s *userService) UpdatePassword(id types.Snowflake, newPassword string) error {
+	if newPassword == "" {
+		return errors.New("password is required")
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(newPassword), bcrypt.DefaultCost)
+	if err != nil {
+		return errors.New("failed to hash password")
+	}
+
+	return s.userRepo.UpdatePassword(id, string(hash))
+}
+
+func (s *userService) DeleteUser(id types.Snowflake, mediaService MediaService) error {
+	if mediaService != nil {
+		if err := mediaService.DeleteAllFromUser(id); err != nil {
+			return err
+		}
+	}
+	return s.userRepo.Delete(id)
+}
--- backend/utils/utils.go
+++ backend/utils/utils.go
@@ -3,3 +3,16 @@ package utils
 func IntPtr(i int) *int {
 	return &i
 }
+
+func IfNotNilPointer[T any](newValue, defaultValue *T) *T {
+	if newValue != nil {
+		return newValue
+	}
+	return defaultValue
+}
+func IfNotNilValue[T any](newValue *T, defaultValue T) T {
+	if newValue != nil {
+		return *newValue
+	}
+	return defaultValue
+}
