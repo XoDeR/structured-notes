2250e7e8f8a50d905f9b697c346b8e499bc5bb3f
XoDeR <xoder.vs@gmail.com>
2025-11-25 18:27:54 +0100

    session service

--- backend/services/session.service.go
+++ backend/services/session.service.go
@@ -1,1 +1,21 @@
 package services
+
+import "structured-notes/repositories"
+
+type SessionService interface {
+	DeleteOldSessions() error
+}
+
+type sessionService struct {
+	sessionRepo repositories.SessionRepository
+}
+
+func NewSessionService(sessionRepo repositories.SessionRepository) SessionService {
+	return &sessionService{
+		sessionRepo: sessionRepo,
+	}
+}
+
+func (s *sessionService) DeleteOldSessions() error {
+	return s.sessionRepo.DeleteOld()
+}
