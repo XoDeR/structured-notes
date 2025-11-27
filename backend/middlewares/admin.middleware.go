104cfab6e7fb3c37f8d772b9d83a9aea4f7934b7
XoDeR <xoder.vs@gmail.com>
2025-11-27 21:08:00 +0100

    auth and admin middlewares -- check if token is valid and if there's a corresponding role for admin

---
+++ backend/middlewares/admin.middleware.go
@@ -0,0 +1,25 @@
+package middlewares
+
+import (
+	"structured-notes/permissions"
+	"structured-notes/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Admin() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		role, ok := c.Get("user_role")
+		if !ok {
+			c.JSON(401, utils.Error("Invalid role in token"))
+			c.Abort()
+			return
+		}
+		if role != permissions.RoleAdministrator {
+			c.JSON(401, utils.Error("Unauthorized"))
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
---
+++ backend/middlewares/auth.middleware.go
@@ -0,0 +1,58 @@
+package middlewares
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+	"structured-notes/permissions"
+	"structured-notes/types"
+	"structured-notes/utils"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type AuthClaims struct {
+	Role string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func Auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString, err := c.Cookie("Authorization")
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, utils.Error("Bad access token."))
+			c.Abort()
+			return
+		}
+
+		claims := AuthClaims{}
+		token, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, http.ErrAbortHandler
+			}
+			return []byte(os.Getenv("JWT_SECRET")), nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, utils.Error("Bad access token."))
+			c.Abort()
+			return
+		}
+		user_id, err := strconv.ParseUint(claims.Subject, 10, 64)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, utils.Error("Invalid user ID"))
+			c.Abort()
+			return
+		}
+		user_role, err := strconv.Atoi(claims.Role)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, utils.Error("Invalid user role"))
+			c.Abort()
+			return
+		}
+		c.Set("user_id", types.Snowflake(user_id))
+		c.Set("user_role", permissions.UserRole(user_role))
+		c.Next()
+	}
+}
