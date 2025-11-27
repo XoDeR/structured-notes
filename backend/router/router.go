3882624170d8e10e5d04a85237fd463893e4c3b4
XoDeR <xoder.vs@gmail.com>
2025-11-27 21:22:11 +0100

    added cors handler, added all routes to the router

--- backend/router/router.go
+++ backend/router/router.go
@@ -1,13 +1,33 @@
 package router
 
 import (
+	"os"
 	"structured-notes/app"
+	"structured-notes/router/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter(app *app.App) *gin.Engine {
 	router := gin.New()
 
+	router.Use(gin.Recovery())
+
+	router.SetTrustedProxies([]string{"127.0.0.1", "localhost"})
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{os.Getenv("DOMAIN_CLIENT")},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
+
+	mainGroup := router.Group("/api")
+	routes.Users(app, mainGroup)
+	routes.Auth(app, mainGroup)
+	routes.Uploads(app, mainGroup)
+	routes.Nodes(app, mainGroup)
+	routes.Permissions(app, mainGroup)
 	return router
 }
