ee32dc9f03be501b6b27f0e151b0f63a061e2272
XoDeR <xoder.vs@gmail.com>
2025-11-28 16:55:42 +0100

    login/signup navigation

--- frontend/app/pages/(public)/login/index.vue
+++ frontend/app/pages/(public)/login/index.vue
@@ -1,3 +1,20 @@
 <template>
   Login
+  <NuxtLink to="/signup" class="signup-link">Need an account? Sign up</NuxtLink>
 </template>
+<style scoped lang="scss">
+.signup-link {
+  display: block;
+  font-weight: 500;
+  color: var(--primary);
+  text-align: center;
+  transition: all 0.2s ease;
+  margin-bottom: 1rem;
+  text-decoration: none;
+
+  &:hover {
+    color: var(--primary-dark);
+    text-decoration: underline;
+  }
+}
+</style>
--- frontend/app/pages/(public)/signup.vue
+++ frontend/app/pages/(public)/signup.vue
@@ -1,3 +1,20 @@
 <template>
   Sign Up
+  <NuxtLink to="/login" class="login-link">Already have an account? Log in</NuxtLink>
 </template>
+<style scoped lang="scss">
+.login-link {
+  display: block;
+  margin: 1.5rem 0;
+  font-weight: 500;
+  color: var(--primary);
+  text-align: center;
+  transition: all 0.2s ease;
+  text-decoration: none;
+
+  &:hover {
+    color: var(--primary-dark);
+    text-decoration: underline;
+  }
+}
+</style>
