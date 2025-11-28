95ca65d8d8a73a01bc4b5b3f8beb4bc1e2000ed4
XoDeR <xoder.vs@gmail.com>
2025-11-28 13:43:11 +0100

    public home page skeleton with a link to /dashboard

--- frontend/app/pages/(public)/index.vue
+++ frontend/app/pages/(public)/index.vue
@@ -1,4 +1,45 @@
+<script setup lang="ts">
+</script>
+
 <template>
+  <main>
+    <section>
+      <div class="buttons">
+        <NuxtLink :prefetch="false" to="/dashboard" class="btn primary glow">Get Started</NuxtLink>
+      </div>
+    </section>
+  </main>
   <h1>Home Page</h1>
   <p>Structured Notes</p>
 </template>
+
+<style scoped lang="scss">
+main {
+  padding: 0 2rem;
+  margin-top: 1em;
+  overflow-x: hidden;
+}
+
+.buttons {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 1.5rem;
+}
+
+.btn {
+  padding: 0.9rem 1.8rem;
+  border-radius: 50px;
+  font-weight: 600;
+  text-decoration: none;
+}
+
+.btn.primary {
+  color: var(--primary);
+  background: white;
+}
+
+.btn.glow {
+  box-shadow: 0 10px 30px rgb(0 0 0 / 20%), 0 0 0 rgb(255 255 255 / 60%);
+}
+</style>
