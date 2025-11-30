34160af52953645f60fbabdd1a0d40747bd0d371
XoDeR <xoder.vs@gmail.com>
2025-11-30 16:00:03 +0100

    added /dashboard/categories and dashboard/docs pages

--- frontend/app/pages/(public)/dashboard/home.vue
+++ frontend/app/pages/(public)/dashboard/home.vue
@@ -4,4 +4,10 @@
     <Icon name="material-symbols:perm-media-outline-rounded" />
     <span>Media</span>
   </NuxtLink>
+  <NuxtLink to="/dashboard/categories" class="app">
+    <span>Categories</span>
+  </NuxtLink>
+  <NuxtLink to="/dashboard/docs" class="app">
+    <span>Documents</span>
+  </NuxtLink>
 </template>
